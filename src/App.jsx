df0474d7412901806d2e07f59a776de5d0f077ae
rwijayat <ranjeeva.wijayatunge@sscinc.com>
2026-07-02 12:25:04 +1000

    added sermon

--- src/App.jsx
+++ src/App.jsx
@@ -137,7 +137,7 @@ const sendContactEmail = async (e) => {
 <div className="sermon-card">
 
   <img
-    src={`${import.meta.env.BASE_URL}kathnan.JPG`}
+    src={`${import.meta.env.BASE_URL}kathnan.jpg`}
     alt="Recent Dhamma Sermon"
     className="sermon-image"
   />
